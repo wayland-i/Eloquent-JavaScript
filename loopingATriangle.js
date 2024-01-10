c6ed593fc55889141d9985c715e0765d942f6be1
Ian Iversen <ianiversen1@gmail.com>
2024-01-09 20:43:01 -0500

    solution: looping a triangle

--- loopingATriangle.js
+++ loopingATriangle.js
@@ -1,6 +1,6 @@
-for (let i = 1; i < 8; i++) {
-      console.log("#".repeat(i))
-}
+// for (let i = 1; i < 8; i++) {
+//       console.log("#".repeat(i))
+// }
 
 // OR //
 
@@ -9,3 +9,15 @@ for (let i = 1; i < 8; i++) {
 // for (let i = 1; i < 8; i++) {
 //     console.log(pound.repeat(i))
 // }
+
+// Nested for loop, += operator appending string Solution //
+
+let pound = "#"
+
+for (let i = 0; i < 7; i++) {
+      for (let j = 0; j < i; j++) {
+            pound += "#"
+      }
+      console.log(pound)
+      pound = "#"
+}
