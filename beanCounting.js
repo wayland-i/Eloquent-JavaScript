d08eec6b22878453203be0881f1c0417df869d98
Ian Iversen <ianiversen1@gmail.com>
2024-01-10 12:13:00 -0500

    solution: bean counting part 2

--- beanCounting.js
+++ beanCounting.js
@@ -1,20 +1,37 @@
 // write a function called countBs that takes 1 argument (a string) and returns the number of "B" characters there are in that string
 
 
-// First solution
-let bCounter = 0
+// Solution for first problem
+// let bCounter = 0
 
-function countBs( myString) {
+// function countBs( myString) {
+//     for (let i = 0; i <= myString.length - 1; i++) {
+//         if (myString[i] == "B") {
+//             bCounter += 1
+//         }
+//     }
+//     return bCounter;
+// }
+
+// const count = countBs("BaBBy back RiBs")
+
+// console.log(count)
+
+
+
+// Solution for second problem
+
+let charCounter = 0
+
+function countChars( myString, char) {
     for (let i = 0; i <= myString.length - 1; i++) {
-        if (myString[i] == "B") {
-            bCounter += 1
+        if (myString[i] == char) {
+            charCounter += 1
         }
     }
-    return bCounter;
+    return charCounter;
 }
 
-const count = countBs("BaBBy back RiBs")
+const count = countChars("BaBBy back RiBs", "a")
 
 console.log(count)
-
-
