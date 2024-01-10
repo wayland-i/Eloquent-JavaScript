20faf2e7cddede9bf4c710b0aa18fe07e1a5c6f3
Ian Iversen <ianiversen1@gmail.com>
2024-01-10 15:08:05 -0500

    review: get properties on a string

---
+++ getProperties.js
@@ -0,0 +1,26 @@
+// get properties on a datatype that you have defined
+
+
+
+// const myString = "hello"
+
+// let stringObject = new String(myString)
+
+// let properties = Object.getOwnPropertyNames(stringObject);
+
+// console.log(properties)
+
+
+
+// let stringPrototypeProperties = Object.getOwnPropertyNames(String.prototype)
+
+// console.log(stringPrototypeProperties)
+
+
+
+// let myString = "hello"
+
+// console.log(myString.length)
+// .length isn't a method, it is just a property so it doesn't require invocation
+
+
