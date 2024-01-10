d8351b13e2706f28f8d2c95550ca5bb5f5fe9432
Ian Iversen <ianiversen1@gmail.com>
2024-01-10 10:40:54 -0500

    solution: minimum

---
+++ minimum.js
@@ -0,0 +1,20 @@
+// MATH.min returns its smallest argument
+// write a function that takes two arguments and returns the smaller of the two
+
+
+// first solution
+// function min(x, y) {
+//     console.log(Math.min(x, y))
+// }
+// min(5, 16)
+
+
+
+// second solution
+// let min = Math.min(5, 2)
+// console.log(min)
+
+
+// third solution
+const min = (x, y) => Math.min(x, y)
+console.log(min(8, 22))
