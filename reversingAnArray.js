5f6b3f16efb0db76407ca35794d9f023e72d2afb
Ian Iversen <ianiversen1@gmail.com>
2024-01-10 21:52:42 -0500

    solution: second part of reversing an array

--- reversingAnArray.js
+++ reversingAnArray.js
@@ -3,13 +3,33 @@
 
 let myArray = [1, 2, 3, 4, 5]
 
-const reverseArray = (array) => {
-    let reverseArray = []
-    for (let num of array) {
-        reverseArray.unshift(num)
-    }  
-    return reverseArray
+// const reverseArray = (array) => {
+//     let reverseArray = []
+//     for (let num of array) {
+//         reverseArray.unshift(num)
+//     }  
+//     return reverseArray
+// }
+
+// console.log(reverseArray(myArray))
+
+
+// write a function that reverses an array without creating a new array
+// this had to utilize a temp variable to store the value that would in the next line be
+// overwritten. That temp number would then go to the end of the array
+
+const reverseArrayInPlace = (array) => {
+    let start = 0
+    let end = array.length - 1
+    while (start < end) {
+        let temp = array[start]
+        array[start] = array[end]
+        array[end] = temp
+
+        start++
+        end--
+    }
+    return array
 }
 
-console.log(reverseArray(myArray))
-
+console.log(reverseArrayInPlace(myArray))
