ae1548df6a4b45b3877e061e31fe53b87ecb1c1c
Ian Iversen <ianiversen1@gmail.com>
2024-01-15 11:30:52 -0500

    solution: aList first part

--- AList.js
+++ AList.js
@@ -1,3 +1,22 @@
 // write a function that takes an array as an argument and creates an obj of objects based on the number
 // of elements in the array
 
+myArray = [1, 2, 3]
+
+const arrayToList = (array) => {
+    let list = {}
+    for (let i = array.length - 1; i >= 0; i--) {
+        // console.log(array[i])
+        if (i == array.length - 1)
+        {
+            list = {value: array[i], rest: null}
+            // console.log(list)
+        } else if (i < array.length - 1) {
+            list = {value: array[i], rest: list}
+            // console.log(list)
+        }
+    }
+    return list
+}
+
+console.log(arrayToList(myArray))
