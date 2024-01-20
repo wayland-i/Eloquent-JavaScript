c52ff5b6096e4277d7abaf3882ce8719490dbe3f
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 14:17:38 -0500

    feature: added notes on great notes on reference types and mutability of types

---
+++ referenceTypes.js
@@ -0,0 +1,31 @@
+
+//primitive data types in JS
+let num = 5
+let bool = true
+let string = "hello" //strings deep down are really ref types. they can be acted upon using some array methods. STRINGS ARE IMMUTABLE (biggest takeaway)
+console.log(num)
+
+//"reference" data types in JS... 
+let array = [1, 2, 3]
+let myObj = {mood: "happy"}
+
+
+
+let myString = "hello world"
+
+
+let y = new Number(6)
+
+console.log(y)
+
+let z = new Boolean(true)
+
+console.log(z)
+
+let a = new String("Hello")
+
+console.log(a)
+
+let b = new Object({book: "Peter Pan"})
+
+console.log(b)
