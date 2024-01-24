11b073e52e8e0a489cd850500d4c472978fd379f
Ian Iversen <ianiversen1@gmail.com>
2024-01-23 21:36:02 -0500

    Solution: flattening solved, not using reduce or concat methods

--- flattening.js
+++ flattening.js
@@ -3,22 +3,35 @@
 let myArray = [1, 2, 3, 4]
 let myArray1 = [5, 6, 7, 8]
 
-let superArray = [[1, 2, 3, 4], [5, 6, 7, 8]]
+let superArray = [[1, 2, 3, 4,], [5, 6, 7, 8, 9]]
 
 // console.log(myArray.reduce((a, b) => a + b))
 
-// console.log(myArray.concat(myArray1))
+console.log(myArray.concat(myArray1))
 
 // console.log(superArray.reduce((a, b) => a + b))
 
-console.log(superArray.length - 1)
+// console.log(superArray.length - 1)
 
-let elems = superArray.length - 1 
+// let elems = superArray.length - 1 
 
-let newArray = [];
+// let newArray = [];
 
-for (let i = 0; i <= elems; i++) {
-    console.log(superArray[i])
-    newArray += superArray[i]
+// for (let i = 0; i <= elems; i++) {
+//     console.log(superArray[i])
+//     newArray += superArray[i]
     
+// }
+
+
+this is a solution that doesn't use reduce or concat but still works
+const newArray = new Array;
+
+for (let i = 0; i < superArray.length; i ++) {
+    for (let j = 0; j < superArray[i].length; j++) {
+        console.log(superArray[i][j])
+        newArray.push(superArray[i][j])
+    }
 }
+
+console.log(newArray)
--- referenceTypes.js
+++ referenceTypes.js
@@ -1,18 +1,22 @@
 
 //primitive data types in JS
-let num = 5
-let bool = true
-let string = "hello" //strings deep down are really ref types. they can be acted upon using some array methods. STRINGS ARE IMMUTABLE (biggest takeaway)
+let num = 5 // Numbers are immutable
+let bool = true // Booleans are immutable
+// STRINGS ARE IMMUTABLE in JS, C, and JAVA
+let string = "hello" //strings deep down are really ref types (C, Java). they can be acted upon using some array methods in JS. 
+// STRINGS ARE IMMUTABLE in JS, C, JAVA
+// when you "edit a string" you are really creating a new string in memory
 console.log(num)
 
 //"reference" data types in JS... 
 let array = [1, 2, 3]
+// arrays were meant to be rearranged and updated, as a design choice they were made to be mutable
 let myObj = {mood: "happy"}
+// objs can be either. but are often mutable unless they are set to be immutable
 
 
 
 let myString = "hello world"
-
 
 let y = new Number(6)
 
@@ -26,6 +30,9 @@ let a = new String("Hello")
 
 console.log(a)
 
-let b = new Object({book: "Peter Pan"})
+let b = new Object({book: "Peter Pan"}) // this is mutable
+let c = new Object.freeze({movie: "Hook"}) // this was made to be immutable by the .freez method
+//when you edit variable c you create a new object in memory.
 
 console.log(b)
+console.log(c)
