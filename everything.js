ff62ae4e6589b46a685043d6e2bff975e14f9fd8
Ian Iversen <ianiversen1@gmail.com>
2024-01-28 18:12:41 -0500

    solution: everything first part

---
+++ everything.js
@@ -0,0 +1,17 @@
+implement every as a function that takes an array and predicate function as parameters
+
+const everyFunction = (array, predFunction) => {
+    return predFunction(array)
+}
+
+console.log(everyFunction([1, 2, 3, 4], predFunction))
+
+function predFunction(x) {
+    return x.every((num) => {
+        console.log(num > 0)
+        return num > 0
+    })
+}
+
+
+// implement one version using a loop
