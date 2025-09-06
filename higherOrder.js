32d5689b58975802034bd069cd3cd359cf18e715
Ian Iversen <ianiversen1@gmail.com>
2025-09-05 22:23:25 -0400

    filter

--- higherOrder.js
+++ higherOrder.js
@@ -1,7 +1,20 @@
-//filter
+//higher order functions!
 
+//higher order functions = either take functions as arguments or return functions. OR both!
+
+//a callback function = a function that is passed as an arguement to another function
+
+//filter
+//filter takes a callback function that returns true or false for each item, if it returns true, then the item stays in the new array. If not it is filtered out.
+const numbersInFilterArray = [1, 2, 3, 4, 5]
+const evens = numbersInFilterArray.filter(n => n % 2 === 0)
+console.log(evens)
+
+//
 
 //map
+
+
 
 //reduce
 
