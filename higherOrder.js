8591497ef2ea6e123c59fd766a733a3b4024be9e
Ian Iversen <ianiversen1@gmail.com>
2025-09-05 22:40:12 -0400

    map

--- higherOrder.js
+++ higherOrder.js
@@ -10,11 +10,12 @@ const numbersInFilterArray = [1, 2, 3, 4, 5]
 const evens = numbersInFilterArray.filter(n => n % 2 === 0)
 console.log(evens)
 
-//
-
 //map
-
-
+//map takes a callback function that returns a NEW array. It performs essentially a forEach, but forEach does not inherently return an array, forEach just causes side effets.
+//forEach always returns undefined, unless you tell it to explicitly return ~something~... it's just for logging or for showing things in the DOM really. It's really just an end point for an array.
+const numsForMap = [1, 2, 3, 4]
+const doubled = numsForMap.map(x => x * 2)
+console.log(doubled)
 
 //reduce
 
